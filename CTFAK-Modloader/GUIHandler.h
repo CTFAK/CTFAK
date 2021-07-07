ba8adbd8b97916b6d555926a3e9f44eed0549853
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2021-07-07 19:40:04 +0700

    Remove C++ project
    This is unnesscarry project for now.

--- CTFAK-Modloader/GUIHandler.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <d3d9.h>
-
-
-long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice);
-DWORD WINAPI HookDirect3D9(LPVOID lpParam);
--- CTFAK-Modloader/Settings.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-namespace Settings
-{
-	bool disableEvents;
-};
